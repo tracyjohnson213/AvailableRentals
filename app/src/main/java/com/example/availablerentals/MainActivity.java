abf9ed01f3452804cf1b4f4335c9c54a812b5ad8
tracyjohnson213 <53531005+tracyjohnson213@users.noreply.github.com>
2019-11-05 19:13:46 -0800

    Initial commit with theme and ic_launcher

---
+++ app/src/main/java/com/example/availablerentals/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.availablerentals;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.mipmap.ic_launcher);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+    }
+}
