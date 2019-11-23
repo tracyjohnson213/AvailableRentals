8befaadb4eeb6ea3958918d57c64619fade32de2
tracyjohnson213 <53531005+tracyjohnson213@users.noreply.github.com>
2019-11-23 08:35:17 -0800

    add about us activity

--- app/src/main/java/com/example/availablerentals/AboutUsActivity.java
+++ app/src/main/java/com/example/availablerentals/AboutUsActivity.java
@@ -2,36 +2,24 @@ package com.example.availablerentals;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.BaseAdapter;
 import android.widget.Button;
-import android.widget.GridView;
-import android.widget.ImageView;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class AboutUsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_about_us);
 
-        // set home page icon launcher
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
-        getSupportActionBar().setLogo(R.mipmap.ic_launcher);
-        getSupportActionBar().setDisplayUseLogoEnabled(true);
-
-        // go to splash activity after click of button
+        // go to property badge activity after click of button
         Button viewRentalsButton = (Button)findViewById(R.id.btnViewRentals);
         viewRentalsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this,SplashActivity.class));
+                startActivity(new Intent(AboutUsActivity.this,PropertyBadgeActivity.class));
             }
         });
 
@@ -40,17 +28,17 @@ public class MainActivity extends AppCompatActivity {
         rentalCriteriaButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this,RentalCriteriaActivity.class));
+                startActivity(new Intent(AboutUsActivity.this,RentalCriteriaActivity.class));
             }
         });
 
-        // go to about us activity after click of button
-        Button aboutUsButton = (Button)findViewById(R.id.btnAboutUs);
+        // go to rental criteria activity after click of button
+        /*Button aboutUsButton = (Button)findViewById(R.id.btnEmail);
         aboutUsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this,AboutUsActivity.class));
+                startActivity(new Intent(AboutUsActivity.this,EmailActivity.class));
             }
-        });
+        });*/
     }
 }
