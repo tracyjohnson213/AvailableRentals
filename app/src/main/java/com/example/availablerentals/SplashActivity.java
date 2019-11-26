43097b8bb70f88876c6b84f9fdb6bc582efcfe19
tracyjohnson213 <53531005+tracyjohnson213@users.noreply.github.com>
2019-11-26 15:13:42 -0800

    use persistent data to populate property info

--- app/src/main/java/com/example/availablerentals/SplashActivity.java
+++ app/src/main/java/com/example/availablerentals/SplashActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -15,6 +16,18 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        // set text for address
+        int position = 3;
+        TextView txtPropertyAddress = (TextView)findViewById(R.id.txtSpecialAddress);
+        txtPropertyAddress.setText(Variables.Addresses[position][0]);
+        TextView txtPropertyZip = (TextView)findViewById(R.id.txtSpecialZip);
+        txtPropertyZip.setText(Variables.Addresses[position][1]);
+        TextView txtPropertyBeds = (TextView)findViewById(R.id.txtSpecialBed);
+        txtPropertyBeds.setText(Variables.Unitsizes[position][0] + "/" + Variables.Unitsizes[position][1]);
+        TextView txtPropertyPrice = (TextView)findViewById(R.id.txtSpecialPrice);
+        txtPropertyPrice.setText(Variables.Unitsizes[position][2]);
+
+        // set timer to close activity
         TimerTask task = new TimerTask() {
             @Override
             public void run() {
