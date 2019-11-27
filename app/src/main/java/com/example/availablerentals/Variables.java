78d8457150fcce328bda03fee40c6d07652cb847
tracyjohnson213 <53531005+tracyjohnson213@users.noreply.github.com>
2019-11-27 08:23:02 -0800

    add property

--- app/src/main/java/com/example/availablerentals/Variables.java
+++ app/src/main/java/com/example/availablerentals/Variables.java
@@ -7,7 +7,7 @@ public class Variables {
             {R.drawable.eoceanblvd1075front,R.drawable.eoceanblvd1075kitchen,R.drawable.eoceanblvd1075bath},
             {R.drawable.e4thst3225front,R.drawable.e4thst3225livingroom,R.drawable.e4thst3225kitchen},
             {R.drawable.walnutave589front,R.drawable.walnutave589kitchen,R.drawable.walnutave589bath},
-            {R.drawable.walnutave589front}
+            {R.drawable.cedarwalk40livingroom,R.drawable.cedarwalk40kitchen,R.drawable.cedarwalk40bedroom,R.drawable.cedarwalk40bath}
     };
     static String[][] Address = {
             {"1162 E 3rd St", "90802"},
@@ -15,7 +15,7 @@ public class Variables {
             {"1075 E Ocean Blvd", "90802"},
             {"3225 E 4th St","90814"},
             {"589 Walnut Ave","90802"},
-            {"589 Walnut Ave","90802"}
+            {"40 Cedar Walk","90802"}
     };
     static String[][] Unitsize = {
             {"studio","1 bath", "$1250"},
@@ -23,6 +23,6 @@ public class Variables {
             {"1 bed","1 bath", "$1895"},
             {"2 beds","1 bath", "$2150"},
             {"3 beds", "2 baths", "$3800"},
-            {"3 beds", "2 baths", "$3800"}
+            {"3 beds", "2 baths", "$3270"}
     };
 }
